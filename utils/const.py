2039b0a4336e8f8997a765ee43ab43485f487d8b
Mukul Sharma <mukulsharma.0462@gmail.com>
2020-11-23 01:16:36 +0530

    Update const.py

--- utils/const.py
+++ utils/const.py
@@ -5,5 +5,5 @@ JWT_EXPIRATION_TIME_MINUTES = 60
 USERNAME = 'mukul'
 PASSWORD = '$2b$12$FvOYr/V0PE5ldyKzhCfHcOoNvEgvgPKS/HvjHVg2s4yqoETXIAS5m'
 
-MODEL_PATH = r'E:\fastapi_practice\simple_ml\services\lin_reg_california_housing_model.joblib'
+MODEL_PATH = r'/app/services/lin_reg_california_housing_model.joblib'
 MULTIPLIER = 100000
